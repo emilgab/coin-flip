59f21a31e59d3d7235a44021cf00aa5690936ed6
EmilG <10610649+emilgab@users.noreply.github.com>
2019-02-15 14:50:56 +0100

    Updated and added checks
    Added checks for inputs and optimised the code a little.

--- coin-flip.py
+++ coin-flip.py
@@ -4,7 +4,6 @@ import random
 def coin_flip(players_choice, amount):
     """
     This function flips a coin and returns if a player won.
-    If the player won, the amount of money he gets back is doubled.
     INPUT: players choice, bet amount
     """
     # Assigns a random value (0 or 1) to flip_result
@@ -13,7 +12,7 @@ def coin_flip(players_choice, amount):
     # Animating the loading / coin flip with a pause at 0.2s for each print
     coin_animation = ["––","\\","|","/","––","\\","|","/","––"]
     for i in range(0,9):
-        print("\n"*100)
+        print("\n"*30)
         print("Flipping coin... ", coin_animation[i])
         time.sleep(0.2)
         
@@ -23,21 +22,25 @@ def coin_flip(players_choice, amount):
     else:
         return f"Sorry, you lost {amount}"
 
-# Assigns values to the input
-choice = input('Please choose "heads" or "tails": ')
-bet = int(input('Please choose the amount to bet: '))    
-    
-# Checks the input and assigns a numeric value
+# Assigns values to the input, and checks for correct input
+check = False
+
+while check == False:
+    choice = input("Please write HEADS or FALSE: ")
+    if choice == "heads" or choice == "tails":
+        check = True
+
 while True:
-    # Checks for what input and assigns a number based on input
-    if choice.lower() == "heads":
-        choice = 0
+    try:
+        bet = int(input("Please choose the amount to bet: "))
         break
-    elif choice.lower() == "tails":
-        choice = 1
-        break
-    else:
-        print("Wrong input, please try again.")
+    except:
         continue
+
+# Checks for what input and assigns a number based on input
+if choice == "heads":
+    choice = 0
+elif choice == "tails":
+    choice = 1
     
 print(coin_flip(choice, bet))
